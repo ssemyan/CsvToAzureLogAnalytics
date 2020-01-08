5689d24206adada0ec0f437dcf9830b2d9bb7afe
Scott Semyan <semyan@hotmail.com>
2020-01-08 13:04:58 -0800

    Initial commit.

---
+++ LogAnalyticsDataCollector.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToAzureLogAnalytics
+{
+	public class LogAnalyticsDataCollector
+	{
+		// Log Analytics workspace ID
+		private readonly string _workspaceId;
+
+		// For sharedKey, use either the primary or the secondary Connected Sources client authentication key   
+		private readonly string _workspaceKey;
+
+		// LogName is name of the event type that is being submitted to Azure Monitor
+		private readonly string _logName;
+
+		// You can use an optional field to specify the timestamp from the data. If the time field is not specified, Azure Monitor assumes the time is the message ingestion time
+		private readonly string _timeStampField;
+
+		public LogAnalyticsDataCollector(string customerId, string sharedKey, string logName, string timeStampField)
+		{
+			_workspaceId = customerId;
+			_workspaceKey = sharedKey;
+			_logName = logName;
+			_timeStampField = timeStampField;
+		}
+
+		// Build the API signature
+		private static string BuildSignature(string message, string secret)
+		{
+			var encoding = new ASCIIEncoding();
+			byte[] keyByte = Convert.FromBase64String(secret);
+			byte[] messageBytes = encoding.GetBytes(message);
+			using (var hmacsha256 = new HMACSHA256(keyByte))
+			{
+				byte[] hash = hmacsha256.ComputeHash(messageBytes);
+				return Convert.ToBase64String(hash);
+			}
+		}
+
+		// Send a request to the POST API endpoint
+		public async Task<HttpResponseMessage> PostData(string json)
+		{
+			// Create a hash for the API signature
+			var datestring = DateTime.UtcNow.ToString("r");
+			var jsonBytes = Encoding.UTF8.GetBytes(json);
+			string stringToHash = "POST\n" + jsonBytes.Length + "\napplication/json\n" + "x-ms-date:" + datestring + "\n/api/logs";
+			string hashedString = BuildSignature(stringToHash, _workspaceKey);
+			string signature = "SharedKey " + _workspaceId + ":" + hashedString;
+
+			string url = "https://" + _workspaceId + ".ods.opinsights.azure.com/api/logs?api-version=2016-04-01";
+
+			HttpClient client = new HttpClient();
+			client.DefaultRequestHeaders.Add("Accept", "application/json");
+			client.DefaultRequestHeaders.Add("Log-Type", _logName);
+			client.DefaultRequestHeaders.Add("Authorization", signature);
+			client.DefaultRequestHeaders.Add("x-ms-date", datestring);
+			client.DefaultRequestHeaders.Add("time-generated-field", _timeStampField);
+
+			HttpContent httpContent = new StringContent(json, Encoding.UTF8);
+			httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			return await client.PostAsync(new Uri(url), httpContent);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+
+namespace CsvToAzureLogAnalytics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get filename from arguments
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage:\n\tCsvToAzureLogAnalytics.exe [name of csv file to process]");
+                return;
+            }
+            var fileName = args[0];
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine($"File {fileName} does not exist");
+                return;
+            }
+
+            // Get values from config
+            var config = new ConfigurationBuilder()
+             .SetBasePath(AppContext.BaseDirectory)
+             .AddJsonFile("appsettings.json", optional: false)
+             .Build();
+
+            var workspaceId = config["WorkspaceId"];
+            var workspaceKey = config["WorkspaceKey"];
+            var logName = config["LogName"];
+            var timeStampField = config["TimeStampField"];
+            var batchSizeStr = config["BatchSize"];
+
+            // Make sure config set correctly
+            if (string.IsNullOrEmpty(workspaceId) || string.IsNullOrEmpty(workspaceKey) || string.IsNullOrEmpty(logName) || string.IsNullOrEmpty(timeStampField) || string.IsNullOrEmpty(batchSizeStr))
+            {
+                Console.WriteLine("Values missing from appsettings.json");
+                return;
+            }
+
+            // Make sure batch size is an int
+            if (!int.TryParse(batchSizeStr, out int batchSize))
+            {
+                Console.WriteLine("BatchSize is not a number");
+                return;
+            }
+
+            // Create Data Collector
+            var collector = new LogAnalyticsDataCollector(workspaceId, workspaceKey, logName, timeStampField);
+
+            string[] properties = null;
+            using (TextFieldParser fieldParser = new TextFieldParser(fileName))
+            {
+                // Configure the TextFieldParser
+                // This tells the parser it is a Delimited text 
+                fieldParser.TextFieldType = FieldType.Delimited;
+                // This tells the parser that delimiter used is a ,
+                fieldParser.Delimiters = new string[] { "," };
+                // This tells the parser that some fields may have quotes around them
+                fieldParser.HasFieldsEnclosedInQuotes = true;
+                // Used to hold the fields after each read
+                string[] fields;
+
+                // Read and process the fields
+                int totalCount = 0;
+                int curBatchSize = 0;
+                var listObjResult = new List<Dictionary<string, object>>();
+                while (!fieldParser.EndOfData)
+                {
+                    // Parse the line and read into the array
+                    fields = fieldParser.ReadFields();
+                    if (properties == null)
+                    {
+                        properties = fields;
+                    }
+                    else
+                    {
+                        totalCount++;
+                        var objResult = new Dictionary<string, object>();
+                        for (int j = 0; j < properties.Length; j++)
+                        {
+                            // check if record is number or datetime
+                            string rec = fields[j];
+                            if (double.TryParse(rec, out double rec_d))
+                            {
+                                // Record is number
+                                objResult.Add(properties[j], rec_d);
+                            }
+                            else if (DateTime.TryParse(rec, out DateTime rec_t))
+                            {
+                                // Record is datetime
+                                objResult.Add(properties[j], rec_t);
+                            }
+                            else
+                            {
+                                // Record is string
+                                objResult.Add(properties[j], rec);
+                            }
+                        }
+
+                        listObjResult.Add(objResult);
+                        curBatchSize++;
+
+                        // Send out current batch 
+                        if (curBatchSize >= batchSize)
+                        {
+                            SendLogs(collector, listObjResult, totalCount);
+                            listObjResult.Clear();
+                            curBatchSize = 0;
+                        }
+                    }
+                }
+                // Send any logs
+                SendLogs(collector, listObjResult, totalCount);
+            }
+        }
+
+        private static void SendLogs(LogAnalyticsDataCollector collector, List<Dictionary<string, object>> listObjResult, int totalCount)
+        {
+            // Convert to JSON
+            string jsonOut = JsonConvert.SerializeObject(listObjResult);
+            
+            // Send
+            Console.Write($"Sending {totalCount}...");
+            var result = collector.PostData(jsonOut).Result;
+            result.EnsureSuccessStatusCode();
+            Console.WriteLine($" done.");
+        }
+    }
+}
